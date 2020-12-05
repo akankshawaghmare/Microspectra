92cd3cf8ab86c8df4c08e212dcd98937ec80eede
akankshawaghmare <75319641+akankshawaghmare@users.noreply.github.com>
2020-12-05 11:39:10 +0530

    Add files via upload

---
+++ day1 Introduction/firstprogram.py
@@ -0,0 +1,9 @@
+print("hello world")
+name="akanksha"
+middlename=("sanjay")
+surname=("waghmare")
+print(name,middlename,surname)
+print(name[0:9:2]) #slicing
+a="program"
+b= type (a)
+print(b) #type of the variable
